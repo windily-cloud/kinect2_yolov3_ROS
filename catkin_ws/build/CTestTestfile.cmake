1d18342f77f9aefc76f4f1e7fa9488cc103e6e50
windily <www.4399li@qq.com>
2020-04-21 17:25:24 +0800

    complete kinect test

---
+++ catkin_ws/build/CTestTestfile.cmake
@@ -0,0 +1,12 @@
+# CMake generated Testfile for 
+# Source directory: /home/xll/windily/catkin_ws/src
+# Build directory: /home/xll/windily/catkin_ws/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs(gtest)
+subdirs(iai_kinect2/iai_kinect2)
+subdirs(iai_kinect2/kinect2_registration)
+subdirs(iai_kinect2/kinect2_bridge)
+subdirs(iai_kinect2/kinect2_calibration)
+subdirs(iai_kinect2/kinect2_viewer)
---
+++ catkin_ws/build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export PKG_CONFIG_PATH="/home/xll/turbot_ws/devel/lib/pkgconfig:/opt/ros/kinetic/lib/pkgconfig:/opt/ros/kinetic/lib/x86_64-linux-gnu/pkgconfig"
+export PWD="/home/xll/windily/catkin_ws/build"
+export PYTHONPATH="/home/xll/turbot_ws/devel/lib/python2.7/dist-packages:/opt/ros/kinetic/lib/python2.7/dist-packages"
